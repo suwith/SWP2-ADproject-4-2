33ec59a853b25b05bbf210732634f32ea25a91d9
suwith <suwith@kookmin.ac.kr>
2021-12-14 02:19:30 +0900

    Update trender.py

--- trender.py
+++ trender.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+    QGridLayout, QApplication, QLabel, QTextBrowser, QSizePolicy)
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
